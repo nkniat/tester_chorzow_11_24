b274d626a1ad3474e8fe8ddaee0c26f033c4a87f
nkniat <101292925+nkniat@users.noreply.github.com>
2024-11-23 10:52:02 +0100

    Adding folders with exercices

---
+++ Code Review/validate_pesel.py
@@ -0,0 +1,22 @@
+def validate_pesel(pesel):
+    """
+    Funkcja walidująca numer PESEL.
+    PESEL powinien mieć 11 cyfr i spełniać warunki kontroli poprawności.
+    """
+    if len(pesel) != 11:  # Sprawdzenie długości PESEL
+        return False  # Nie zwraca informacji, dlaczego nieprawidłowy
+
+    if not pesel.isdigit():  # Sprawdzenie, czy PESEL zawiera tylko cyfry
+        return "Nieprawidłowy PESEL: zawiera niedozwolone znaki"
+
+    weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+    checksum = 0
+
+    for i in range(10):  # Pętla obliczająca sumę kontrolną
+        checksum += int(pesel[i]) * weights[i]  # Brak uwzględnienia wyjątków, np. dla cyfr 0
+
+    control_digit = 10 - (checksum % 10)  # Obliczenie cyfry kontrolnej
+    if control_digit != int(pesel[10]):  # Błąd w sprawdzeniu cyfry kontrolnej
+        return False
+
+    return "PESEL poprawny"  # Nieadekwatny typ zwracanej wartości
---
+++ Test Cases Trello/readme.txt
@@ -0,0 +1,19 @@
+# Projekt Trello
+
+Aplikacja do zarządzania zadaniami dla zespołów.
+
+## Wymagania
+- System logowania i rejestracji
+- Tworzenie, edytowanie i usuwanie zadań
+- Możliwość filtrowania i sortowania zadań
+
+## Instalacja
+1. Klonuj repozytorium:
+git clone https://github.com/nazwa_uzytkownika/repo.git
+
+2. Zainstaluj zależności:
+npm install
+
+
+## Testowanie
+Testy dla aplikacji można znaleźć w folderze `test-cases`.
